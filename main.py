22a04de9f5f674a785aa4da376db98fd104af2a7
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-11 11:42:40 -0400

    cors

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
 import crud
@@ -11,24 +10,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-
-origins = [
-
-    "http://localhost:3000",  
-    "https://fast-api-tutorial-backend.vercel.app/",
-    "https://fastapi-tutorial.netlify.app"
-    "https://fastapi-tutorial.netlify.app/tutorial"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
-
 
 def get_db():
     db = SessionLocal()
@@ -48,10 +36,6 @@ def read_ceo(ceo_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="CEO not found")
     return db_ceo
 
-
 @app.post("/ceos/{ceo_id}", response_model=schemas.CEO)
 def create_ceo(ceo_id: int, ceo: schemas.CEOCreate = Body(...), db: Session = Depends(get_db)):
-    created_ceo = crud.create_ceo(db=db, ceo=ceo)
-    response = JSONResponse(content=created_ceo)
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    return response
+    return crud.create_ceo(db=db, ceo=ceo)
