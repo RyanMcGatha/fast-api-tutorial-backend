ae79c27fcbd59cb2dcda6e67acc72b2f752ee409
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-13 15:52:07 -0400

    update

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def get_db():
@@ -43,6 +43,6 @@ def read_ceo_by_name(ceo_name: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="CEO not found")
     return db_ceo
 
-@app.post("/ceos/{ceo_id}", response_model=schemas.CEO)
+@app.post("/ceos/id/{ceo_id}", response_model=schemas.CEO)
 def create_ceo(ceo_id: int, ceo: schemas.CEOCreate = Body(...), db: Session = Depends(get_db)):
     return crud.create_ceo(db=db, ceo=ceo)
