c4366ff42934375e52bc5de415930ed01be7aba4
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-19 00:30:51 -0400

    setup db pooling

--- crud.py
+++ crud.py
@@ -6,7 +6,10 @@ def get_ceo_by_id(db: Session, ceo_id: int):
     return db.query(models.CEO).filter(models.CEO.id == ceo_id).first()
 
 def get_ceo_by_name(db: Session, ceo_name: str):
-    return db.query(models.CEO).filter(models.CEO.name == ceo_name).first()
+    db_ceo = db.query(models.CEO).filter(models.CEO.name == ceo_name).first()
+    if db_ceo is None:
+        return None
+    return db_ceo
 
 
 def create_ceo(db: Session, ceo: schemas.CEOCreate):
