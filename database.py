a8d16470dd1f3e9a911211a75cb912a9af2ceade
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-19 00:02:29 -0400

    setup db pooling

--- database.py
+++ database.py
@@ -11,8 +11,16 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 DEV_DATABASE_URL = os.getenv("DEV_DATABASE_URL")
 
 
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,          
+    max_overflow=5,       
+    pool_timeout=30,       
+    pool_recycle=1800,     
+    pool_pre_ping=True     
+)
 
-
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 Base = declarative_base()
